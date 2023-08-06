56c0f5b02cf9b663943d1536fbfd5d062b3e87e2
Nathnael Dereje <nathandere1357@gmail.com>
2023-08-07 01:00:05 +0300

    Fix startServer logic
    Fix the logic steps to start server by doing connection to database and listening in different functions and doing database connection first. This will ensure that if the database connection fails the program exits sommthly without starting to listen through a port.

--- backend/app.js
+++ backend/app.js
@@ -35,21 +35,35 @@ mongoose.set('strictQuery', false);
 
 async function connectToDb() {
     try {
+        console.log(`trying to Connect to database...`);
         await mongoose.connect(process.env.MONGODBURL, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log(`Connected to database successfully`);
     } catch (error) {
-        console.log(`Database Error: ${error.message}`);
+        throw new Error(`Database Error: ${error.message}`);
     }
 }
 
-async function startServer() {
+
+async function startListening(){
     try {
+        console.log(`Trying to start listening through port ${process.env.PORT}...`);
         app.listen(process.env.PORT);
-        console.log((`Server listening through port ${process.env.PORT}`));
-        connectToDb();
+        console.log(`Server listening through port ${process.env.PORT}`);
     } catch (error) {
-        console.log(`Server Error: ${error.message}`);
+        throw new Error(error.message);
     }
 }
+
+
+async function startServer() {
+    try {
+        await connectToDb();
+        await startListening();
+        console.log(`✔ Server started successfully ✔`);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
 
 startServer();
